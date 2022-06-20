df43fe5e4c695e9054346924cf365c02c1555454
duannx <xuanduannguyen@gmail.com>
2022-06-20 17:34:09 +0700

    npm: add different build targets

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,17 @@
 import typescript from 'rollup-plugin-typescript2';
 export default {
     input: "src/index.ts",
-    output: {
-        dir: 'dist',
-        format: 'es',
-    },
+    output: [
+        {
+            file: "dist/index.es.js",
+            format: "es",
+        },
+        {
+            file: "dist/index.cjs.js",
+            format: "cjs",
+            exports: "default"
+        }
+    ],
     plugins: [
         typescript(
             {
