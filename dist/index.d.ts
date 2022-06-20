81d0b6e6b9189eae6616e6efd8a27e97643c00e4
duandz <du02@goldenphoenix.vn>
2022-06-20 10:19:37 +0700

    feat: add type declaration

---
+++ dist/index.d.ts
@@ -0,0 +1,18 @@
+declare const _default: import("vue").DefineComponent<{
+    isShow: {
+        type: BooleanConstructor;
+        default: any;
+    };
+}, () => import("vue").VNode<import("vue").RendererNode, import("vue").RendererElement, {
+    [key: string]: any;
+}> | import("vue").VNode<import("vue").RendererNode, import("vue").RendererElement, {
+    [key: string]: any;
+}>[], unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    isShow: {
+        type: BooleanConstructor;
+        default: any;
+    };
+}>>, {
+    isShow: boolean;
+}>;
+export default _default;
